13ad21edbc0f59f10145ff9e3b08153c0d7280da
Kervin Rianzares <kfrianzares@student.apc.edu.ph>
2019-09-13 08:35:34 +0800

    no message

---
+++ lab1/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE HTML>
+<!--
+	Aqueous by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Cy</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:700italic,400,300,700' rel='stylesheet' type='text/css'>
+		<!--[if lte IE 8]><script src="js/html5shiv.js" type="text/javascript"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+	</head>
+	<body class="homepage">
+
+	<!-- Header -->
+	<div id="header-wrapper">
+		<div id="header">
+			<div class="container">
+				<div id="logo"> <!-- Logo -->
+					<h1><a href="#">Cyrelle Medina</a></h1>
+					<span></span>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<!-- Header -->
+		
+	<!-- Main -->
+		<div id="main-wrapper">
+			
+			<div id="banner">
+				<div class="container">
+					<div class="row">
+						<section>
+						<br><br><br>
+							<a href="#" class="image full"><img src="meep.jpg" alt=""></a>
+						</section>
+					</div>
+				</div>
+			</div>
+
+			<div class="container">
+				<div id="main">
+	
+					<div class="row">
+					
+						<!-- Content -->
+							<div id="content" class="8u skel-cell-important">
+								<section>
+									<header>
+										<h2>Introduction</h2>
+										<span class="byline"></span>
+									</header>
+									<p><font size = "5px"> Joeun achimimnida! I'm <strong> Cyrelle Medina</strong> I just turned 19 last month.<a href="http://templated.co"></a> I live in Pasay City near NAIA Terminal 1.<a href="http://templated.co/license"></a> My course is BSIT-MI because my father wants me to achieve this course.<a href="http://templated.co"></a></a>.</font></p>
+									<p><font size = "5px"> I graduated senior high school at Malate Catholic School. I'm also a member of MCS Phoenix Pep Squad. I'm interested to KPOP since 2009 that is why I always watch their dance practices and support them always. Dancing really saved my life because that's the only hobby I always do if I'm bored. Also, I want to travel the world when I have a chance because I love to explore new cultures and things in every country.</font></p>
+									<p><font size = "5px"> I just enjoyed my freshman year because I met a lot of friends and especially being part of APC-DC I learned a lot of genres in dancing. I just spend my summer just in my house to take care of my grandmother and also, I attend Dance Series in Asia Pacific College.</font></p>
+				
+								</section>
+							</div>
+							
+						<!-- Sidebar -->
+							<div id="sidebar" class="4u">
+								<section>
+									<header>
+										<h2>Memories </h2>
+										<span class="byline"></span>
+									</header>
+									<ul class="style1">
+										<li class="first">
+											<p class="date"><a href="#">April<b>04</b></a></p>
+											<p><a class="image full"><img src="apcdc.jpg" alt=""></a></p>
+										</li>
+										<li>
+											<p class="date"><a href="#">March<b>24</b></a></p>
+											<p><a class="image full"><img src="pep.jpg" alt=""></a></p>
+										</li>
+										<li>
+											<p class="date"><a href="#">September<b>05</b></a></p>
+											<p><a class="image full"><img src="mi.jpg" alt=""></a></p>
+										</li>
+									</ul>
+														
+								</section>
+							</div>
+	
+					</div>
+				
+				</div>
+			</div>
+		</div>
+	<!-- /Main -->
+
+	
+
+	<!-- Copyright -->
+		<div id="copyright">
+			<div class="container">
+				Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)
+			</div>
+		</div>
+
+
+	</body>
+</html>
